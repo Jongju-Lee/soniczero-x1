114010b2458ef037f4aa93365f57d98a9362b98d
jongju-Lee <jeje7@naver.com>
2026-03-06 12:37:20 +0900

    design: support 페이지의 support search 반응형 작업 완료.

--- src/sections/support/SupportHero.jsx
+++ src/sections/support/SupportHero.jsx
@@ -6,13 +6,14 @@ const SupportHero = () => {
   const sectionRef = useScrollFadeIn(false, { delay: 0.2 });
 
   return (
-    <section className="support__hero section-lg" ref={sectionRef}>
-      <div className='supprot__hero-header'></div>
-      <div className="support__hero-badge">Support Center</div>
-      <h1 className="support__hero-title">HOW CAN WE HELP?</h1>
-      <p className="support__hero-desc">SonicZero X1에 대한 모든 궁금증을 해결해 드립니다.</p>
+    <section className="support__search" ref={sectionRef}>
+      <div className='support__search-intro intro'>
+        <div className="support__search-intro-badge">Support Center</div>
+        <h1 className="support__search-intro-title">HOW CAN WE HELP?</h1>
+        <p className="support__search-intro-desc">SonicZero X1에 대한 <span>모든 궁금증을 해결해 드립니다.</span></p>
+      </div>
 
-      <div className="support__search">
+      <div className="support__search-wrapper">
         <div className="support__search-bar">
           <img src="/assets/icons/support-magnifier.svg" alt="" className="support__search-icon" />
           <input type="text" placeholder="도움이 필요하신 내용을 검색해 보세요." />
